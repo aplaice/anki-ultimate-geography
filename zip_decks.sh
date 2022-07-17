a765a2d08975f16983afd6edd99e42803930a049
Adam Plaice <plaice.adam+github@gmail.com>
2022-07-17 19:48:51 +0200

    Avoid hardcoding deck zip names

--- zip_decks.sh
+++ zip_decks.sh
@@ -2,9 +2,17 @@
 
 version="${1:-everfresh}"
 
+build_paths=""
+
 cd build/ || exit 1
 for lang_code in CS DE EN ES FR NB NL PL PT RU SV ZH
 do
-    zip -r "Ultimate_Geography_${version}_${lang_code}.zip" "Ultimate Geography [${lang_code}]"/
-    zip -r "Ultimate_Geography_${version}_${lang_code}_EXTENDED.zip" "Ultimate Geography [${lang_code}] [Extended]"/
+    standard_zip="Ultimate_Geography_${version}_${lang_code}.zip"
+    extended_zip="Ultimate_Geography_${version}_${lang_code}_EXTENDED.zip"
+    zip -r "$standard_zip" "Ultimate Geography [${lang_code}]"/
+    zip -r "$extended_zip" "Ultimate Geography [${lang_code}] [Extended]"/
+    build_paths="$build_paths build/$standard_zip build/$extended_zip"
 done
+
+# Magic cookie for setting value https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#example-setting-a-value
+echo '::set-output name=DECK_PATHS::'"${build_paths}"
