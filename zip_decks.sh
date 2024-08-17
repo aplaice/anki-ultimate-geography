638b317ee8d19267c40fb6aaa8eef280cdf66dd4
Adam Plaice <plaice.adam+github@gmail.com>
2024-08-17 15:06:56 +0200

    Add experimental/interactive decks to zip_decks.sh

--- zip_decks.sh
+++ zip_decks.sh
@@ -11,9 +11,11 @@ for lang_code in $LANG_CODES
 do
     standard_zip="Ultimate_Geography_${version}_${lang_code}.zip"
     extended_zip="Ultimate_Geography_${version}_${lang_code}_EXTENDED.zip"
+    experimental_zip="Ultimate_Geography_${version}_${lang_code}_EXPERIMENTAL.zip"
     zip -r "$standard_zip" "Ultimate Geography [${lang_code}]"/
     zip -r "$extended_zip" "Ultimate Geography [${lang_code}] [Extended]"/
-    build_paths="$build_paths build/$standard_zip build/$extended_zip"
+    zip -r "$experimental_zip" "Ultimate Geography [${lang_code}] [Experimental]"/
+    build_paths="$build_paths build/$standard_zip build/$extended_zip build/$experimental_zip"
 done
 
 # Magic cookie for setting value https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#example-setting-a-value
